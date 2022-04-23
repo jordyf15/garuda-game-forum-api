e846ecf0d8f047d3b9f54b3d90146ee15b1725f0
jordyf15 <jordyferdian88@gmail.com>
2022-04-23 15:49:01 +0700

    created likes table migration

---
+++ migrations/0000000000006_create-table-likes.js
@@ -0,0 +1,24 @@
+exports.up = (pgm) => {
+  pgm.createTable('likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'comments',
+      onDelete: 'CASCADE',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('likes');
+};

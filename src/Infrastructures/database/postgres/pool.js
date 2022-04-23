9cb6c5d4db6ce80cd80f3ebb750aaebee04e4bd1
jordyf15 <jordyferdian88@gmail.com>
2022-04-23 11:30:07 +0700

    test

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -7,9 +7,8 @@ const testConfig = {
   user: process.env.PGUSER_TEST,
   password: process.env.PGPASSWORD_TEST,
   database: process.env.PGDATABASE_TEST,
-  sslmode: 'require',
 };
 
-const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool('postgres://tfcsxskubwzkuv:740e1e06dd441fa62aa8996d3e8d0542b10f2587899fd2b84d442fb4e7c13e66@ec2-3-224-125-117.compute-1.amazonaws.com:5432/d46piu4af049j5');
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
 
 module.exports = pool;

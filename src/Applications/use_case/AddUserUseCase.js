05aba65b0eeab20b54fc4203fbeacf805e966fd7
jordyf15 <jordyferdian88@gmail.com>
2022-04-23 10:47:53 +0700

    added logs for heroku debugging

--- src/Applications/use_case/AddUserUseCase.js
+++ src/Applications/use_case/AddUserUseCase.js
@@ -7,8 +7,11 @@ class AddUserUseCase {
   }
 
   async execute(useCasePayload) {
+    console.log('baru masuk use case');
     const registerUser = new RegisterUser(useCasePayload);
+    console.log('mau pake db');
     await this._userRepository.verifyAvailableUsername(registerUser.username);
+    console.log('wow aman');
     registerUser.password = await this._passwordHash.hash(registerUser.password);
     return this._userRepository.addUser(registerUser);
   }
